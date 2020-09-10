dae81b1221535d38ad6dbe3249bf568551af1b04
Takumaron <tennis.takumax@gmail.com>
2020-09-10 18:24:50 +0900

    jsonを返すHeaderを定義

--- handler/server.go
+++ handler/server.go
@@ -8,5 +8,6 @@ import (
 func Hello(w http.ResponseWriter, r *http.Request) {
 	values := r.URL.Query()
 
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, "Hello!, %s", values.Get("text"))
 }
