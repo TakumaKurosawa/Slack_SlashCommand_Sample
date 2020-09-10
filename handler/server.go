01aee38951c2f349bccd35eb8f923708fa96388c
Takumaron <tennis.takumax@gmail.com>
2020-09-10 18:05:57 +0900

    ユーザーのインプットを受け取れるサーバーに実装

--- handler/server.go
+++ handler/server.go
@@ -1,10 +1,30 @@
 package handler
 
 import (
+	"Slack_SlashCommand_Sample/reqbody"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello!")
+	body := r.Body
+	defer body.Close()
+
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, body); err != nil {
+		log.Println(err)
+	}
+
+	log.Println(r.Body)
+
+	var reqBody reqbody.UserInput
+	if err := json.Unmarshal(buf.Bytes(), &reqBody); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Fprintf(w, "Hello!, %s", reqBody.Text)
 }
