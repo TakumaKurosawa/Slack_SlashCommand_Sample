e660b1f07d33f9749c68ace57a055921a6ad638d
Takumaron <tennis.takumax@gmail.com>
2020-09-12 14:58:47 +0900

    omitempty前に空白があってはいけない？？

--- response/res.go
+++ response/res.go
@@ -7,7 +7,7 @@ type SlackMessage struct {
 type SlackBlock struct {
 	Type      string          `json:"type"`
 	Text      SlackText       `json:"text"`
-	Accessory *SlackAccessory `json:"accessory, omitempty"`
+	Accessory *SlackAccessory `json:"accessory,omitempty"`
 }
 
 type SlackText struct {
