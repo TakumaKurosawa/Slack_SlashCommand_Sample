0fbaf5c24013a46ffb20f56bc0ab6046b41ace9a
Takumaron <tennis.takumax@gmail.com>
2020-09-12 15:03:17 +0900

    サンプルデータを追加してみた

--- handler/server.go
+++ handler/server.go
@@ -17,18 +17,42 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 				Type: "section",
 				Text: response.SlackText{
 					Type: "mrkdwn",
-					Text: fmt.Sprintf("*%s*", values.Get("text")),
+					Text: fmt.Sprintf("*input: %s*へのおすすめメンターランキング", values.Get("text")),
 				},
 			},
 			{
 				Type: "section",
 				Text: response.SlackText{
 					Type: "mrkdwn",
-					Text: "https://techbowl.co.jp/techtrain/mentors/78",
+					Text: "<https://techbowl.co.jp/techtrain/mentors/78|黒澤拓磨>\n :star::star::star::star::star:",
 				},
 				Accessory: &response.SlackAccessory{
 					Type:     "image",
 					ImageURL: "https://techbowl-production.s3.ap-northeast-1.amazonaws.com/mentor-profile-image/27b892da10905f353cf1a6835440a089.jpg",
+					AltText:  "hoge",
+				},
+			},
+			{
+				Type: "section",
+				Text: response.SlackText{
+					Type: "mrkdwn",
+					Text: "<https://techbowl.co.jp/techtrain/mentors/79|石塚 崇寛>\n :star::star::star::star:",
+				},
+				Accessory: &response.SlackAccessory{
+					Type:     "image",
+					ImageURL: "https://techbowl-production.s3.ap-northeast-1.amazonaws.com/mentor-profile-image/9b6428a47821e88eded480ce2377669b.jpg",
+					AltText:  "hoge",
+				},
+			},
+			{
+				Type: "section",
+				Text: response.SlackText{
+					Type: "mrkdwn",
+					Text: "<https://techbowl.co.jp/techtrain/mentors/67|白川 みちる>\n :star::star::star:",
+				},
+				Accessory: &response.SlackAccessory{
+					Type:     "image",
+					ImageURL: "https://techbowl-production.s3.ap-northeast-1.amazonaws.com/mentor-profile-image/81f80b447359317aadad05102fc39759.jpg",
 					AltText:  "hoge",
 				},
 			},
